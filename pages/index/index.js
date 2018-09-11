76e3c4da8bf9632f0dd61104d57525afa47a7bf8
unknown <unknown@example.com>
2018-09-11 16:09:32 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,15 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  onLoad: function () {
+    
+  },
+  getUserInfo: function(e) {
+
+  },
+  onShareAppMessage(options) {
+    console.log(options.webViewUrl)
+  }
+})
